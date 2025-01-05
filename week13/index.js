da5da878674d7aa3bfc052474f6e7a19c071ae5a
ungalianta <zannagervald@gmail.com>
2025-01-05 20:25:44 +0300

    last changes

--- week13/index.js
+++ week13/index.js
@@ -1,43 +1,46 @@
-document.getElementById('calculate').onclick = function() {
-    const input = document.getElementById('birthday').value;
-    const errorMessage = document.getElementById('error-message');
+document.getElementById('calculate').onclick = function(){
+    const inputBirthday = document.getElementById('birthday').value;
+    const error = document.getElementById('error-message');
     const result = document.getElementById('result');
 
-    if (!input) {
-        errorMessage.style.display = 'block';
-        result.textContent = '';
-        return;
-    } 
-    
-    else {
-        errorMessage.style.display = 'none';
-    }
+if (!inputBirthday) {
+    error.style.display = 'block';
+    result.textContent = '';
+    return
+}
+else{
+    error.style.display = 'none'
+}
 
-    const today = new Date();
-    const birthDay = new Date(today.getFullYear(), new Date(input).getMonth(), new Date(input).getDate());
+const today = new Date();
+const birthday = new Date(today.getFullYear(), new Date(inputBirthday).getMonth(), new Date(inputBirthday).getDate());
 
-    if (birthDay < today) {
-        birthDay.setFullYear(today.getFullYear() + 1);
-    }
+if (birthday < today) {
+birthday = today.getFullYear() +1;
+}
 
-    const dateDiff = birthDay - today; 
-    const daysDiff = Math.floor(dateDiff / (1000 * 3600 * 24)); 
+const timeDiff = birthday - today;
+const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
 
-    let dayWord = "дней";
-    if (daysDiff % 10 === 1 && daysDiff % 100 !== 11) {
-        dayWord = "день";
-    } 
-    
-    else if (daysDiff % 10 >= 2 && daysDiff % 10 <= 4 && (daysDiff % 100 < 12 || daysDiff % 100 > 14)) {
-        dayWord = "дня";
-    }
+let dayWord = "дней";
+if (daysDiff % 10 === 1 && daysDiff % 100 !== 11) {
+    dayWord = "день";
+} else if (daysDiff % 10 >= 2 && daysDiff % 10 <= 4 && (daysDiff % 100 < 12 || daysDiff % 100 > 14)) {
+    dayWord = "дня";
+}
 
-    result.textContent = `До вашего дня рождения осталось ${daysDiff} ${dayWord}.`;
+result.textContent = `До вашего дня рождения осталось ${daysDiff} ${dayWord}.`;
 };
 
-document.getElementById('birthday').oninput = function() {
+    document.getElementById('birthday').oninput = function() {
     const errorMessage = document.getElementById('error-message');
+    
     if (this.value) {
-        errorMessage.style.display = 'none';
+    errorMessage.style.display = 'none';
     }
-}
+};
+
+
+
+
+
