776aa135b85135795944a78a9c6b0c2ec82225c6
ungalianta <zannagervald@gmail.com>
2025-04-25 15:23:04 +0300

    week21

---
+++ week21/my-app/src/App.js
@@ -0,0 +1,28 @@
+import { Chart } from "react-google-charts";
+import './App.css';
+
+function App() {
+  const data = [
+    ["Task", "Hours per Day"],
+    ["Work", 9],
+    ["Eat", 2],
+    ["Commute", 2],
+    ["Watch TV", 2],
+    ["Sleep", 7],
+  ];
+
+  const options = {
+    title: "My Daily Activities",
+  };
+  return (
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
+  );
+}
+
+export default App;
